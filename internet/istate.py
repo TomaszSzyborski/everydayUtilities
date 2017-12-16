0afb68c88771bb16e072932097eb27e3702c84e6
Amr Essam <amressam01@gmail.com>
2017-12-16 16:11:05 +0200

    istate updates

--- internet/istate.py
+++ internet/istate.py
@@ -1,19 +1,19 @@
-import os
-import time
-import sys
+from os import system
+from time import sleep
+from sys import argv, platform
 
 try:
 	while True:
-		x = os.system('ping google.com -c 4 -t 3')
+		x = system('ping google.com -c 4 -t 3')
 		
 		if x == 0:
 			print("up")
-			os.system("say internet is up") #macOnly
+			if platform == 'darwin': system("say internet is up")
 		else:
 			print("down")
-			os.system("say internet is down") #macOnly
+			if platform == 'darwin': system("say internet is down")
 		
-		time.sleep(int(sys.argv[1]) if len(sys.argv) > 1 else 57)
+		sleep(int(argv[1]) if len(argv) > 1 else 27)
 
 except KeyboardInterrupt:
 	print()
