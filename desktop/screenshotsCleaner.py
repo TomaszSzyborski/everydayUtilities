da5b641f627724baceafa23d83da9d65434db7d1
Amr Essam <amressam01@gmail.com>
2017-12-16 15:30:20 +0200

    code cleaning

--- desktop/screenshotsCleaner.py
+++ desktop/screenshotsCleaner.py
@@ -1,35 +1,26 @@
-import os
-import sys
+from os import path, listdir, rename, system
+from sys import argv
 
 #--setup--
-desktop_dir = os.path.join(os.path.expanduser('~'), 'Desktop') #linux and mac only
-imgs_dir = os.path.join(desktop_dir, 'imgs') #your screenshots folder
-listdir = os.listdir(desktop_dir) #list all the files in your desktop
+desktop_dir = path.join(path.expanduser('~'), 'Desktop') #linux and mac only
+imgs_dir = path.join(desktop_dir, 'imgs') #your screenshots folder
+dirlist = listdir(desktop_dir) #list all the files in your desktop
 
 nofiles = True
 count = 0
 
-for i in listdir:
+for i in dirlist:
 	if i.endswith('.png') and 'screen shot' in i.lower(): #adjust the condition for your case
-		nofiles = False; count += 1
-		file_path = os.path.join(desktop_dir, i); new_file_path = os.path.join(imgs_dir, i)
-		os.rename(file_path, new_file_path) #move the file to its new place
+		nofiles = False
+		count += 1
+		file_path = path.join(desktop_dir, i);
+		new_file_path = path.join(imgs_dir, i)
+		rename(file_path, new_file_path) #move the file to its new place
 
 if nofiles:
-	if '-v' in sys.argv:
-		os.system("say \"no screenshots on your desktop.\"") #mac only
-	else:
-		print("no screenshots on your desktop.")
+	message = "no screenshots on your desktop."
+	system("say \"{}\"".format(message)) if '-v' in argv else print(message)
 
 else:
-	if '-v' in sys.argv:
-		os.system("say {} screenshots are cleared from your desktop, have a nice day.".format(count)) #mac only
-	else:
-		print('{} screenshots are cleared from your desktop, have a nice day.'.format(count))
-
-
-'''for i in os.listdir(os.path.join(os.path.expanduser('~'), 'Desktop')):
-	if i.endswith('.png') and 'screen shot' in i.lower():
-		os.rename(os.path.join(os.path.expanduser('~'), 'Desktop', i), os.path.join(os.path.expanduser('~'), 'Desktop', 'imgs',  i))
-'''
-#print(sys.argv)
+	message = "{} screenshots are cleared from your desktop, have a nice day.".format(count)
+	system("say \"{}\"".format(message)) if '-v' in argv else print(message)
